64699ee3297f228ff414e9fd78377e1ead30fb1c
wellingtonctm <w.luiscm@gmail.com>
2025-05-25 10:03:42 -0300

    include databaseloader in the main window

--- leviosa.cpp
+++ leviosa.cpp
@@ -1,7 +1,18 @@
 #include "leviosa.h"
+#include "databaseloader.h"
 
-Leviosa::Leviosa(QWidget *parent)
-    : QMainWindow(parent)
-{}
+#include <QVBoxLayout>
+
+Leviosa::Leviosa(QWidget *parent) : QMainWindow(parent)
+{
+    QWidget *centralWidget = new QWidget(this);
+    QVBoxLayout *layout = new QVBoxLayout;
+    centralWidget->setLayout(layout);
+
+    DatabaseLoader *databaseLoader = new DatabaseLoader(this);
+    layout->addWidget(databaseLoader);
+
+    setCentralWidget(centralWidget);
+}
 
 Leviosa::~Leviosa() {}
