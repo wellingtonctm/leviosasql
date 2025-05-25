3a76dbd15a1fa2fd9f2ce06e2c10df589e4bc181
wellingtonctm <w.luiscm@gmail.com>
2025-05-25 10:01:16 -0300

    create a custom input widget to be reused

---
+++ input.cpp
@@ -0,0 +1,56 @@
+#include "input.h"
+
+#include <QVBoxLayout>
+#include <QRegularExpressionValidator>
+
+Input::Input(QString label, QWidget *parent) : QWidget{parent}
+{
+    QVBoxLayout *layout = new QVBoxLayout;
+    setLayout(layout);
+
+    layout->setContentsMargins(0,0,0,0);
+    label_ = new QLabel(label, this);
+    layout->addWidget(label_);
+    input_ = new QLineEdit(this);
+    layout->addWidget(input_);
+}
+
+void Input::setValidator(QString pattern) {
+    QScopedPointer<QRegularExpressionValidator> validator(new QRegularExpressionValidator(QRegularExpression(pattern), this));
+    input_->setValidator(validator.take());
+}
+
+
+bool Input::validate() {
+    QString str = text();
+    int pos = 0;
+    return input_->validator()->validate(str, pos) == QValidator::Acceptable;
+}
+
+void Input::setEchoMode(QLineEdit::EchoMode mode) {
+    input_->setEchoMode(mode);
+}
+
+QString Input::text() {
+    return input_->text();
+}
+
+void Input::setText(const QString &text) {
+    return input_->setText(text);
+}
+
+void Input::setPlaceholderText(const QString &text) {
+    return input_->setPlaceholderText(text);
+}
+
+void Input::setEnabled(bool enabled) {
+    return input_->setEnabled(enabled);
+}
+
+void Input::setReadOnly(bool readOnly) {
+    return input_->setReadOnly(readOnly);
+}
+
+void Input::clear() {
+    return input_->clear();
+}
---
+++ input.h
@@ -0,0 +1,29 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <QLineEdit>
+#include <QLabel>
+
+#include <QWidget>
+
+class Input : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Input(QString label, QWidget *parent = nullptr);
+    void setValidator(QString pattern);
+    bool validate();
+    void setEchoMode(QLineEdit::EchoMode mode);
+    QString text();
+    void setText(const QString &text);
+    void setPlaceholderText(const QString &text);
+    void setEnabled(bool enabled);
+    void setReadOnly(bool readOnly);
+    void clear();
+
+private:
+    QLabel *label_;
+    QLineEdit *input_;
+};
+
+#endif // INPUT_H
