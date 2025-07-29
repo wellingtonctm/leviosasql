3bec52d207f7fc17f3402552ec328e10b3266eab
wellingtonctm <w.luiscm@gmail.com>
2025-07-28 22:09:08 -0300

    feat: improve error management

--- leviosa.cpp
+++ leviosa.cpp
@@ -4,16 +4,28 @@
 
 #include <QVBoxLayout>
 #include <QSplitter>
+#include <QMessageBox>
+#include <QTimer>
 
-Leviosa::Leviosa(QWidget *parent) : QMainWindow(parent)
+Leviosa::Leviosa(QWidget *parent, SshWorker *sshWorker) : QMainWindow(parent), sshWorker_(sshWorker)
 {
+    if (!sshWorker_) {
+        QMessageBox::warning(this, tr("Connection Error"), tr("No connection data was provided."));
+        QTimer::singleShot(0, this, &Leviosa::initializationFailed);
+        QTimer::singleShot(0, this, &QWidget::close);
+        return;
+    }
+
     QWidget *centralWidget = new QWidget(this);
     QVBoxLayout *layout = new QVBoxLayout;
     centralWidget->setLayout(layout);
 
+    setWindowTitle(tr("LeviosaSQL (%1@%2)")
+        .arg(sshWorker_->getUser(), sshWorker_->getHost()));
+
     QSplitter *splitter = new QSplitter(Qt::Horizontal, this);
 
-    DatabaseLoader *databaseLoader = new DatabaseLoader(this);
+    DatabaseLoader *databaseLoader = new DatabaseLoader(this, sshWorker_);
     splitter->addWidget(databaseLoader);
 
     MultiTabCodeEditor *codeEditor = new MultiTabCodeEditor(this);
@@ -24,5 +36,3 @@ Leviosa::Leviosa(QWidget *parent) : QMainWindow(parent)
 
     setCentralWidget(centralWidget);
 }
-
-Leviosa::~Leviosa() {}
--- leviosa.h
+++ leviosa.h
@@ -2,13 +2,27 @@
 #define LEVIOSA_H
 
 #include <QMainWindow>
+#include <QThread>
+
+#include "sessionmanagerdialog.h"
+#include "sshworker.h"
 
 class Leviosa : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    Leviosa(QWidget *parent = nullptr);
-    ~Leviosa();
+    Leviosa(QWidget *parent, SshWorker *sshWorker);
+
+    // void OpenSession();
+
+public slots:
+    // void onConnectionResultReceived(bool success, const QString &message);
+
+signals:
+    void initializationFailed();
+
+private:
+    SshWorker *sshWorker_;
 };
 #endif // LEVIOSA_H
